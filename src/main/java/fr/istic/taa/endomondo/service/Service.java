f88785a58bdf37c5b9b97226fad805fe15bf643d
vincent <vincent.bodiou@gmail.com>
2013-11-20 23:05:17 +0100

    change delete

--- src/main/java/fr/istic/taa/endomondo/service/Service.java
+++ src/main/java/fr/istic/taa/endomondo/service/Service.java
@@ -2,6 +2,7 @@ package fr.istic.taa.endomondo.service;
 
 import java.util.Collection;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Query;
 
@@ -11,50 +12,69 @@ import fr.istic.taa.endomondo.model.Seance;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
-public abstract class Service<T> implements IService<T> {
+public abstract class Service<T> implements IService<T>
+{
 
-	private Class<T> type;
+    private Class<T> type;
 
-	public Service() {
-		Type t = getClass().getGenericSuperclass();
-		ParameterizedType pt = (ParameterizedType) t;
-		type = (Class) pt.getActualTypeArguments()[0];
-	}
+    public Service()
+    {
+        Type t = getClass().getGenericSuperclass();
+        ParameterizedType pt = (ParameterizedType) t;
+        type = (Class) pt.getActualTypeArguments()[0];
+    }
 
-	@Override
-	public T create(T obj) {
-		// Begin a transaction
-		EntityTransaction tx = null;
-		try {
-			tx = MyManager.getInstance().getTransaction();
-			tx.begin();
-			MyManager.getInstance().persist(obj);
-			tx.commit();
-			return obj;
-		} catch (RuntimeException e) {
-			if (tx != null && tx.isActive())
-				tx.rollback();
-			return null;
-		}
-	}
+    @Override
+    public T create( T obj )
+    {
+        // Begin a transaction
+        EntityTransaction tx = null;
+        try
+        {
+            tx = MyManager.getInstance().getTransaction();
+            tx.begin();
+            MyManager.getInstance().persist( obj );
+            tx.commit();
+            return obj;
+        }
+        catch ( RuntimeException e )
+        {
+            if ( tx != null && tx.isActive() )
+                tx.rollback();
+            return null;
+        }
+    }
 
-	public T get(int id) {
-		return MyManager.getInstance().find(type, id);
-	}
+    public T get( int id )
+    {
+        return MyManager.getInstance().find( type, id );
+    }
 
-	public int remove(int id) {
-		try {
-			MyManager.getInstance().detach(MyManager.getInstance().find(type, id));
-			return id;
-		} catch (IllegalArgumentException e) {
-			return -1;
-		}
-	}
+    public int remove( int id )
+    {
+        EntityTransaction tx = null;
+        EntityManager manager = MyManager.getInstance();
+        try
+        {
+            tx = manager.getTransaction();
+            tx.begin();
+            Seance seanceToDelete = (Seance) MyManager.getInstance().find( type, id );
+            MyManager.getInstance().remove( seanceToDelete );
+            tx.commit();
+            return id;
+        }
+        catch ( RuntimeException e )
+        {
+            if ( tx != null && tx.isActive() )
+                tx.rollback();
+            return -1;
+        }
+    }
 
-	@SuppressWarnings("unchecked")
-	public Collection<Seance> get() {
-		Query query = MyManager.getInstance().createQuery(
-				"SELECT e FROM " + type.getSimpleName() + " e");
-		return query.getResultList();
-	}
+    @SuppressWarnings( "unchecked" )
+    public Collection<Seance> get()
+    {
+        Query query = MyManager.getInstance().createQuery( "SELECT e FROM " + type.getSimpleName() + " e" );
+        return query.getResultList();
+    }
 }
